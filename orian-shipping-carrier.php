fb0097f91ec1c05b12a1653043f5e95f53dfd187
Samiullah Jamil <jamil.samiullah@gmail.com>
2022-05-28 15:09:50 +0500

    Basic Plugin

---
+++ orian-shipping-carrier.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Orian_Shipping_Carrier
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Orian Shipping Carrier
+Description: This plugin is developed integrate Orian Shipping Carrier API to Woocommerce.
+Author: Samiullah Jamil
+Version: 1.0.0
+Author URI: https://www.samiullahjaml.com/about-me/
+*/
