f0d65b94157d9d1a3820cb8cf09824b9554c4f13
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-06-07 09:49:54 +0500

    Added WC Action and Statuses

---
+++ inc/class-osc-woocommerce-order-actions.php
@@ -0,0 +1,23 @@
+<?php
+if (!class_exists('OSC_Woocommerce_Order_Actions')) {
+    class OSC_Woocommerce_Order_Actions {
+        public $text_domain = "osc";
+        public function __construct() {
+            $this->init();
+        }
+        public function init() {
+            add_action( 'woocommerce_order_actions', array( $this, 'add_order_meta_box_actions' ) );
+            add_action( 'woocommerce_order_action_osc_send_order_to_carrier', array($this, 'osc_process_carrier_order') );
+        }
+        public function add_order_meta_box_actions($actions) {
+            global $theorder;
+            $order_status = $theorder->get_status();
+            if ($order_status === "processing")
+            $actions['osc_send_order_to_carrier'] = __("Send Order to Carrier",$this->text_domain);
+            return $actions;
+        }
+        public function osc_process_carrier_order($order) {
+            $order->update_status("wc-osc-new");
+        }
+    }
+}
---
+++ inc/class-osc-woocommerce-order-status.php
@@ -0,0 +1,48 @@
+<?php
+if (!class_exists('OSC_Woocommerce_Order_Status')) {
+    class OSC_Woocommerce_Order_Status {
+        public $statuses = array(
+            'wc-osc-new' => 'New',
+            'wc-osc-loaded' => 'Loaded',
+            'wc-osc-delivered' => 'Delivered',
+            'wc-osc-lost' => 'Lost',
+            'wc-osc-offloaded' => 'Offloaded',
+            'wc-osc-pickedup' => 'Picked Up',
+        );
+        public $orian_statuses = array(
+            'new' => 'wc-osc-new',
+            'loaded' => 'wc-osc-loaded',
+            'delivered' => 'wc-osc-delivered',
+            'lost' => 'wc-osc-lost',
+            'offloaded' => 'wc-osc-offloaded',
+            'pickedup' => 'wc-osc-pickedup',
+            'canceled' => 'wc-cancelled'
+        );
+        public function __construct() {
+            $this->init();
+        }
+        public function init() {
+            add_action( 'init', array($this,'register_order_statuses') );
+            add_filter( 'wc_order_statuses', array($this,'add_order_statuses') );
+        }
+        public function register_order_statuses() {
+            foreach($this->statuses as $status_key => $status_label) {
+                register_post_status( $status_key, array(
+                    'label'                     => $status_label,
+                    'public'                    => true,
+                    'show_in_admin_status_list' => true,
+                    'show_in_admin_all_list'    => true,
+                    'exclude_from_search'       => false,
+                    'label_count'               => _n_noop( $status_label . ' <span class="count">(%s)</span>', $status_label . ' <span class="count">(%s)</span>' )
+                ) );
+            }
+        }
+        public function add_order_statuses( $order_statuses) {
+            $output_statuses = $order_statuses;
+            foreach ( $this->statuses as $key => $status ) {
+                $output_statuses[ $key ] = $status;
+            }
+            return $output_statuses;
+        }
+    }
+}
