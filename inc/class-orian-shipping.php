3a3f46fe1398f57e3e57329b9f46ee0ef277e9a2
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-06-09 09:20:11 +0500

    Shifted Shipping Methods to Global Class

--- inc/class-orian-shipping.php
+++ inc/class-orian-shipping.php
@@ -23,10 +23,21 @@ if (!class_exists('Orian_Shipping')) {
             include_once dirname(OSC_PLUGIN_FILE) . '/inc/class-osc-woocommerce-order-sync.php';
         }
         public function init() {
+            add_action( 'woocommerce_shipping_init', array($this,'osc_shipping_init') );
+            add_filter( 'woocommerce_shipping_methods', array($this,'osc_add_shipping') );
             $this->api = new OSC_API();
             $this->order_status = new OSC_Woocommerce_Order_Status();
             $this->order_actions = new OSC_Woocommerce_Order_Actions();
             $this->order_sync = new OSC_Woocommerce_Order_Sync();
+        }
+        public function osc_shipping_init() {
+            include_once dirname(OSC_PLUGIN_FILE) . '/inc/shipping-methods/class-osc-delivery-shipping.php';
+            include_once dirname(OSC_PLUGIN_FILE) . '/inc/shipping-methods/class-osc-pudo-shipping.php';
+        }
+        public function osc_add_shipping( $methods ) {
+            $methods['orian_delivery_shipping'] = 'OSC_Delivery_Shipping';
+            $methods['orian_pudo_shipping'] = 'OSC_Pudo_Shipping';
+            return $methods;
         }
     }
 }
