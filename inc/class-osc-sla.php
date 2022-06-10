f1238a23754ed3d517c9235560ee91f418c5309f
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-06-10 16:56:58 +0500

    Added Sla Function to Get Dates according to Type

--- inc/class-osc-sla.php
+++ inc/class-osc-sla.php
@@ -3,13 +3,24 @@ if (!class_exists("OSC_SLA")) {
     class OSC_SLA {
         public $timezone = 'Asia/Jerusalem';
         public $nonbusiness_days;
+        public $home_regular_sla;
+        public $home_far_sla;
+        public $pudo_sla;
         public function __construct() {
             $this->init();
         }
         public function init() {
             $orian_settings = get_option('orian_main_setting');
-            if (isset($orian_settings) && array_key_exists('nonbusiness_days',$orian_settings))
+            if (isset($orian_settings)) {
+                if (array_key_exists('nonbusiness_days',$orian_settings))
             $this->nonbusiness_days = array_map('trim', explode(",",$orian_settings['nonbusiness_days']));
+            if (array_key_exists('delivery_sla',$orian_settings))
+            $this->home_regular_sla = $orian_settings['delivery_sla'];
+            if (array_key_exists('delivery_far_sla',$orian_settings))
+            $this->home_far_sla = $orian_settings['delivery_far_sla'];
+            if (array_key_exists('pickup_sla',$orian_settings))
+            $this->pudo_sla = $orian_settings['pickup_sla'];
+            }
         }
         public function business_days_to_date($days) {
             $response = array();
@@ -31,6 +42,25 @@ if (!class_exists("OSC_SLA")) {
             $weekdays = array('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
             $dayofweek = intval($date->format('w'));
             return $weekdays[$dayofweek] . ' ' . $date->format('d/m');
+        }
+        public function get_delivery_date($sla_type) {
+            $delivery_dates = array();
+            switch ($sla_type) {
+                case 'home':
+                    $numberofdays = $this->home_regular_sla;
+                    break;
+                case 'far':
+                    $numberofdays = $this->home_far_sla;
+                    break;
+                case 'pudo':
+                    $numberofdays = $this->pudo_sla;
+                    break;
+            }
+            if (isset($numberofdays)) {
+            $numberofdays = intval($numberofdays);
+            $delivery_dates = $this->business_days_to_date($numberofdays);
+            }
+            return $delivery_dates;
         }
     }
 }
